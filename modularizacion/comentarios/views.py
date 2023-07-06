c059c031aee911c48f0ec2f2f4e43490c2efcdc7
Nancy <nan_alaobra@hotmail.com>
2023-07-05 23:08:35 -0300

    Delegación de rutas, creación y borrado de datos, estructura y claves foráneas y seeding y paquetes

--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Comment
 
-# Create your views here.
+def test(request):
+    return HttpResponse("Funciona correctamente")
+
+
+def create(request):
+    #comment = Comment(name="Juanjo", score=5, comment="Este es un comentario")
+    #comment.save()
+    comment = Comment.objects.create(name="Alexis")
+    return HttpResponse("Ruta para probar la creación de modulos")
+
+
+def delete(request):
+    #comment =Comment.objects.get(id=1)
+    #comment.delete()
+   
+    Comment.objects.filter(id=13).delete()
+    return HttpResponse("Ruta para probar los borrados")
